15c8f0537097a681f131dd5e8f62a72589d88234
woolfich <peresver@gmail.com>
2025-10-04 17:57:04 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: './grokweld'   // ← твоя подпапка репозитория
+      base: '/grokweld'   // ← твоя подпапка репозитория
     }
   }
 };
